cba2e206e1e9259eb9eec0b8d398018652e4c9a2
Shanthanu S Rai <shanthanu.s.rai9@gmail.com>
2019-03-11 23:20:27 +0530

    Implementation of CacheLine class

--- cache.cpp
+++ cache.cpp
@@ -1,3 +1,27 @@
 //implementation of cache.h goes here
 
 #include "cache.h"
+
+//implementation of CacheLine class
+
+CacheLine::CacheLine(bool valid, int tag, int blockSize) {
+    this->valid = valid;
+    this->tag = tag;
+    this->blockSize = blockSize;
+}
+
+bool CacheLine::getValid() {
+    return valid;
+}
+
+int CacheLine::getTag() {
+    return tag;
+}
+
+void CacheLine::setValid(bool valid) {
+    this->valid = valid;
+}
+
+void CacheLine::setTag(int tag) {
+    this->tag = tag;
+}

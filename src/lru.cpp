83f9fce2ce6b38a89082e32255eeb2b7ec52159e
Shanthanu S Rai <shanthanu.s.rai9@gmail.com>
2019-03-31 20:20:08 +0530

    Modified trace file reading and printing result

--- src/lru.cpp
+++ src/lru.cpp
@@ -16,11 +16,9 @@ ll *lastUsed;
 
 int main(int argc, char *argv[]) {
 
-    vector<ll> addresses = readTrace(argv[1]); //read the trace input file
-
-    ll numberOfSets = atoll(argv[2]); //atoll : char* to long long
-    ll blockSize = atoll(argv[3]);
-    ll setAssociativity = atoll(argv[4]);
+    ll numberOfSets = atoll(argv[1]); //atoll : char* to long long
+    ll blockSize = atoll(argv[2]);
+    ll setAssociativity = atoll(argv[3]);
 
     //lru specific allocation begins
     lastUsed = (ll*) malloc(numberOfSets * setAssociativity * sizeof(ll)); 
@@ -31,9 +29,10 @@ int main(int argc, char *argv[]) {
     //measure time
     auto start = high_resolution_clock::now();
 
-    //go through all addresses
-    for(ll address : addresses) {
-        //address : the current address accessed by CPU
+    while(true) {
+
+        ll address = getNextAddress();
+        if(address == 0) break; // reached eof
 
         ll row; //row in cache
         if((row = cache.isDataInCache(address)) != -1) { //cache hit
@@ -89,16 +88,14 @@ int main(int argc, char *argv[]) {
 
     //measure time
     auto stop = high_resolution_clock::now(); 
-    auto duration = duration_cast<microseconds>(stop - start);
+    auto duration = duration_cast<milliseconds>(stop - start);
 
     //output
-    cout << "Simulation time : " << duration.count() << " ms" << endl;
-    printf("Total Number of data accesses: %lld\n", cache.getNumberOfHits() + cache.getNumberOfMisses());
-    printf("Hits: %lld\n", cache.getNumberOfHits());
-    printf("Misses: %lld\n", cache.getNumberOfMisses());
-    printf("Hit Ratio: %f\n", cache.hitRate() * 100);
-
+    printResult(duration.count(), cache);
+    
+    //lru specific deallocation begins
     free(lastUsed);
+    //lru specific deallocation ends
 
     return 0;
 }

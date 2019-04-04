c571715a23ff0482be198ea7788305866e76cdc8
Shanthanu S Rai <shanthanu.s.rai9@gmail.com>
2019-04-04 13:43:25 +0530

    Renamed P4.c to P3.c

--- matrix/P3.c
+++ matrix/P3.c
@@ -6,7 +6,7 @@
 
 int main() {
 
-    int i, j, k, J, K, r;
+    int i, j, k, I, J, K, r;
     double *A, *B, *C;
 
     A = (double*) calloc(N * N, sizeof(double));
@@ -16,23 +16,25 @@ int main() {
     if(A != NULL && B != NULL && C != NULL){
 
         //printf("Initialising A and B with random values\n");
-        // for(i = 0; i < N; i++) {
-        //     for(int j = 0; j < N; j++) {
-        //         A[i*N + j] = rand()%N;
-        //         B[i*N + j] = rand()%N;
-        //         // C[i][j] = 0;
-        //     }    
-        // }
+        //for(i = 0; i < N; i++) {
+        //    for(int j = 0; j < N; j++) {
+        //        A[i*N + j] = rand()%N;
+        //        B[i*N + j] = rand()%N;
+        //        // C[i][j] = 0;
+        //    }    
+        //}
         //printf("Done\n");
     
         //printf("Performing matrix multiplication\n");
         
-        for(J = 0; J < N; J+= BLOCK){
-            for(K = 0; K < N; K+= BLOCK){
-                for(int i = 0; i < N; i++) {
-                    for(int j = J; j < J + BLOCK; j++) {
-                        for(int k = K; k < K + BLOCK; k++) {
-                            C[i*N + j] += (A[i*N + k] * B[k*N + j]);
+        for(I = 0; I < N; I += BLOCK) {
+            for(J = 0; J < N; J += BLOCK) {
+                for(K = 0; K < N; K += BLOCK) {
+                    for(int i = I; i < I + BLOCK; i++) {
+                        for(int j = J; j < J + BLOCK; j++) {
+                            for(int k = K; k < K + BLOCK; k++) {
+                                C[i*N + j] += (A[i*N + k] * B[k*N + j]);
+                            }
                         }
                     }
                 }

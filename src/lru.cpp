1d6e9d476cdfe6d9cf0ca004c1040ba02882a3a3
Shanthanu S Rai <shanthanu.s.rai9@gmail.com>
2019-06-29 16:12:34 +0530

    Added LRU replacement policy to src

---
+++ src/lru.cpp
@@ -0,0 +1,30 @@
+#include "lru.h"
+
+#define ll long long
+
+LRU::LRU(ll cacheSize, ll blockSize, ll setAssociativity) : 
+    Cache(cacheSize, blockSize, setAssociativity){
+        lastUsed = (ll *)calloc(numberOfSets * (setAssociativity), sizeof(ll));
+}
+
+ll LRU::getBlockToReplace(ll address){
+     int index = getIndex(address);
+     int min_block = index*setAssociativity;
+     for(int block = index*setAssociativity; block < (index+1)*setAssociativity; block++) {
+         if(lastUsed[block] == 0) // an empty slot
+             return block;
+         if(lastUsed[block] < lastUsed[min_block]) {
+             min_block = block;
+         }
+     }
+     return min_block; 
+}
+
+void LRU::update(ll blockToReplace, int status){
+    lastUsed[blockToReplace] = time;
+    time++;
+}
+
+LRU::~LRU(){
+    free(lastUsed);
+}
---
+++ src/lru.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "cache.h"
+
+class LRU : public Cache{
+    private:
+        long long time;
+        long long *lastUsed;
+    public:
+        LRU(long long cacheSize, long long blockSize, long long setAssociativity);
+        long long getBlockToReplace(long long address) override;
+        void update(long long blockToReplace, int status) override;
+
+        ~LRU() override;
+};

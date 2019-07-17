dbd719611af2dd7bdf0d6d19c9e2cca9cf29708f
Rohit M P <rohitmp99@gmail.com>
2019-07-17 14:03:17 +0530

    Constant config file bug fixed

--- run.sh
+++ run.sh
@@ -48,8 +48,8 @@ else
     elif [ $(file --mime-type -b $TRACE) != "application/gzip" ]; then
         echo "Please provide a trace in gzip format"
     else
-        ARGS="$ARGS $(cat params.cfg | awk '/[a-z]/ {print "-D" $1}' | tr '\r\n' ' ' | uniq)"
-        FILES=$(cat params.cfg | awk '/[a-z]/ {print "policies/" $1 ".cpp"}' | tr '\r\n' ' ' | uniq)
+        ARGS="$ARGS $(cat $CONFIG | awk '/[a-z]/ {print "-D" $1}' | tr '\r\n' ' ' | uniq)"
+        FILES=$(cat $CONFIG | awk '/[a-z]/ {print "policies/" $1 ".cpp"}' | tr '\r\n' ' ' | uniq)
         echo $FILES
         make -C ${BASEDIR} G++FLAGS="$ARGS" POLICY_FILES="$FILES"
         gzip -dc $TRACE | ${BASEDIR}/cache.exe $CONFIG

def81395b28ecc9cb35e903639bccdc7d19dfc37
Narayan G <gnarayang@gmail.com>
2019-07-20 15:47:05 +0530

    Added LFU eviction policy

---
+++ policies/lfu.cpp
@@ -0,0 +1,33 @@
+#include "lfu.h"
+
+#define ll long long
+
+LFU::LFU(ll cacheSize, ll blockSize, ll setAssociativity, int level) :
+    Cache(cacheSize, blockSize, setAssociativity, level, "LFU"){
+        timesUsed = (ll *)calloc(numberOfSets * (setAssociativity), sizeof(ll));
+        if(lastUsed == NULL){
+            printf("Failed to allocate memory for data members of LRU (L%d) cache\n", level);
+            exit(0);
+        }
+    }
+
+ll LFU::getBlockToReplace(ll address){
+    int index = getIndex(address);
+    int min_block = index*setAssociativity;
+    for(int block = index*setAssociativity; block < (index+1)*setAssociativity; block++) {
+        if(timesUsed[block] == 0) // an empty slot
+            return block;
+        if(timesUsed[block] < timesUsed[min_block]) {
+            min_block = block;
+        }
+    }
+    return min_block;
+}
+
+void LFU::update(ll block, int status){
+    timesUsed[block]++;
+}
+
+LFU::~LFU(){
+    free(timesUsed);
+}
---
+++ policies/lfu.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../src/cache.h"
+
+class LFU : public Cache{
+    private:
+        long long *timesUsed;
+
+    public:
+        LFU(long long cacheSize, long long blockSize, long long setAssociativity, int level);
+        long long getBlockToReplace(long long address) override;
+        void update(long long blockToReplace, int status) override;
+
+        ~LFU();
+}
